659e3cfaaebc57ac4558d9a20f464090c7a6eeaa
Patrick Salguero Avalos <patrick.salguero.avalos@gmail.com>
2018-02-03 14:12:40 -0500

    Se crea las entidades jpa User & UserRole

---
+++ src/main/java/com/udemy/backendninja/entity/User.java
@@ -0,0 +1,81 @@
+package com.udemy.backendninja.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+	
+	@Id
+	@Column(name="username" , unique=true, nullable=false ,length=45)
+	private String username;
+	
+	@Column(name="password", nullable=false , length=60)
+	private String password;
+	
+	@Column(name="enabled", nullable=false)
+	private boolean enabled;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy="user")
+	private Set<UserRole> userRole = new HashSet<UserRole>();
+
+	public User() {
+
+	}
+
+	public User(String username, String password, boolean enabled) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.enabled = enabled;
+	}
+
+	public User(String username, String password, boolean enabled, Set<UserRole> userRole) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.enabled = enabled;
+		this.userRole = userRole;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public Set<UserRole> getUserRole() {
+		return userRole;
+	}
+
+	public void setUserRole(Set<UserRole> userRole) {
+		this.userRole = userRole;
+	}
+
+}
---
+++ src/main/java/com/udemy/backendninja/entity/UserRole.java
@@ -0,0 +1,64 @@
+package com.udemy.backendninja.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "user_role", uniqueConstraints = @UniqueConstraint(columnNames = { "role", "username" }))
+public class UserRole {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "user_role_id", unique = true, nullable = false)
+	private Integer userRoleId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "username", nullable = false)
+	private User user;
+
+	@Column(name = "role", nullable = false, length = 45)
+	private String role;
+
+	public UserRole() {
+
+	}
+
+	public UserRole(Integer userRoleId, User user, String role) {
+		super();
+		this.userRoleId = userRoleId;
+		this.user = user;
+		this.role = role;
+	}
+
+	public Integer getUserRoleId() {
+		return userRoleId;
+	}
+
+	public void setUserRoleId(Integer userRoleId) {
+		this.userRoleId = userRoleId;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+}
