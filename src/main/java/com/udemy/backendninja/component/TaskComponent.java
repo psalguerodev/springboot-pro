c4555968bae40d11d5833f11c7df014d84e5e873
Patrick Salguero Avalos <patrick.salguero.avalos@gmail.com>
2018-02-03 19:24:22 -0500

    Se implemente Spring Batch / Procesamiento por lotes

---
+++ src/main/java/com/udemy/backendninja/component/TaskComponent.java
@@ -0,0 +1,20 @@
+package com.udemy.backendninja.component;
+
+import java.util.Date;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component("taskComponent")
+public class TaskComponent {
+	
+	private static final Log LOG = LogFactory.getLog(TaskComponent.class);
+	
+	@Scheduled(fixedDelay=5000)
+	public void doTask() {
+		LOG.info("TIME IS: " + new Date());
+	}
+	
+}
